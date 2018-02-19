bb834d46090bba2369e4127dddb06c938305a381
Keyu Lin <mlchaellin222@gmail.com>
2018-02-19 18:31:25 -0500

    added info to general page, and fixed nav css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 export const Header = (props) => {
     return(
-      <nav>
-        <ul>
-          <li><Link to={"/"}>General</Link></li>
-          <li><Link to={"/Maps"}>Maps</Link></li>
-          <li><Link to={"/AboutUs"}>AboutUs</Link></li>
-          <li><Link to={"/Login"}>Login</Link></li>
-        </ul>
-      </nav>
+
+        <nav class="navbar navbar-inverse bg-inverse navbar-default navbar-static-top">
+          <div class="container ">
+          <div class="containercontainer-fluid">
+            <div class="navbar-header mr-5">
+              <a class="navbar-brand text-white" href="/">Congressional Redistrict Generation</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li ><Link to={"/"}>General</Link></li>
+              <li ><Link to={"/Maps"}>Maps</Link></li>
+              <li ><Link to={"/AboutUs"}>AboutUs</Link></li>
+
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li ><Link to={"/Login"}> Login <span class="glyphicon glyphicon-log-in">  </span></Link></li>
+            </ul>
+          </div>
+          </div>
+        </nav>
+
     );
 };
 
