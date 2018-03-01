dc85d86be4bd1581535da67fd92ea1083d8487e1
Keyu Lin <mlchaellin222@gmail.com>
2018-03-01 01:37:42 -0500

    still woking on the map

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 //import components
 import Root from './components/Root';
 import General from './components/General';
-import Map from './components/Map';
+import MapPage from './components/Map';
 import Login from './components/Login';
 import AboutUs from './components/AboutUs';
 
@@ -15,7 +15,7 @@ class App extends Component {
       <BrowserRouter>
         <Root>
           <Route path='/' exact={true} component={General} />
-          <Route path="/Maps" component={Map} />
+          <Route path="/Maps" component={MapPage} />
           <Route path="/AboutUs" component={AboutUs} />
           <Route path="/Login" component={Login} />
         </Root>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,34 +1,53 @@
 import React from 'react';
-import ReactMapboxGl from "react-mapbox-gl";
+import mapboxgl from 'mapbox-gl';
 
-const theMap = ReactMapboxGl({
-  accessToken: 'pk.eyJ1IjoibWxjaGFlbGxpbjIyMiIsImEiOiJjamU4MGQydGYwaW1mMnhtemRqNjZpcm9lIn0.BnWHaQPHz8IPtBXoNW-wzA'
-
-});
-
-export class Map extends React.Component {
+mapboxgl.accessToken ='pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+//const key='pk.eyJ1IjoibWxjaGFlbGxpbjIyMiIsImEiOiJjamU4MGQydGYwaW1mMnhtemRqNjZpcm9lIn0.BnWHaQPHz8IPtBXoNW-wzA';
+export class MapPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      center : [-77.01239, 38.91275],
-
       lng: 5,
       lat: 34,
       zoom: 1.5
     };
   }
   componentDidMount() {
+    const { lng, lat, zoom } = this.state;
 
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom
+    });
+
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
   }
-  render(){
-
-    return(
+  render() {
+    const { lng, lat, zoom } = this.state;
+    const bbb="https://source.unsplash.com/random";
+    return (
       <div>
-        <theMap center={this.state.center}>
-        </theMap>
+        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        </div>
+        <div class="container">
+          <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+          <img src={bbb} />
+        </div>
+
       </div>
-    )
+    );
   }
 };
 
-export default Map;
+export default MapPage;
