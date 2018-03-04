18ba558db0bdf19c7af819d26c88260f84596c7f
Keyu Lin <mlchaellin222@gmail.com>
2018-03-04 03:59:29 -0500

    updated about us form

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 export class AboutUs extends React.Component{
   render(){
     return(
@@ -33,14 +33,22 @@ export class AboutUs extends React.Component{
             <div class="container">
               <div class="col-lg-4 col-lg-offset-1">
                 <form>
-                  <label>Name:</label><br/>
-                  <input type="text" id="name" placeholder="Full name"/><br/>
-                  <label>Email:</label><br/>
-                  <input type="email" id="email" placeholder="example@gmail.com"/><br/>
-                  <label>Feedback:</label><br/>
-                  <input type="textarea" id="feedback" style={{ height: 150, width: 400 }} />
-                  <br/>
-                  <input type="submit" value="Submit" />
+                  <div class="form-group">
+                    <label>Full Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Enter your name" />
+                  </div>
+                  <div class="form-group">
+                    <label>Email address</label>
+                    <input type="email" class="form-control" id="email" placeholder="example@gmail.com" />
+                  </div>
+                  <div class="form-group">
+                    <label>Feedback:</label>
+                    <input type="textarea" class="form-control" id="feedback" style={{ height: 150, width: 400 }}/>
+                  </div>
+                  <div class="form-group">
+                    <Link to={process.env.PUBLIC_URL +'/'}><button type="button" class="btn btn-primary">Submit</button></Link>
+                  </div>
+
                 </form>
               </div>
             </div>
