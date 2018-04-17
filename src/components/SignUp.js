9e5d6b7c71973715e02a2bf558131c3bd6a81141
Keyu Lin <mlchaellin222@gmail.com>
2018-04-17 15:59:13 -0400

    working half way on signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export class Login extends React.Component{
+export class SignUp extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+    this.editFName=this.editFName.bind(this);
+    this.editLName=this.editLName.bind(this);
+    this.editEmail=this.editEmail.bind(this);
+    this.editPassword=this.editPassword.bind(this);
+    this.checkPassword=this.checkPassword.bind(this);
+    this.signUp=this.signUp.bind(this);
+  }
+
   render(){
     return(
       <div className="container">
@@ -12,26 +25,26 @@ export class Login extends React.Component{
           <form>
             <div className="form-group">
               <label>First Name</label>
-              <input type="text" className="form-control" id="loginEmail" placeholder="Enter first name" />
+              <input type="text" className="form-control" id="loginEmail" placeholder="Enter first name" onChange={this.editFName}/>
             </div>
             <div className="form-group">
               <label>Last Name</label>
-              <input type="text" className="form-control" id="loginEmail" placeholder="Enter Last name" />
+              <input type="text" className="form-control" id="loginEmail" placeholder="Enter Last name" onChange={this.editLName}/>
             </div>
             <div className="form-group">
               <label>Email address</label>
-              <input type="email" className="form-control" id="loginEmail" placeholder="Enter email" />
+              <input type="email" className="form-control" id="loginEmail" placeholder="Enter email" onChange={this.editEmail}/>
             </div>
             <div className="form-group">
               <label>Password</label>
-              <input type="text" className="form-control" id="loginPassword" placeholder="Password" />
+              <input type="text" className="form-control" id="loginPassword" placeholder="Password" onChange={this.editPassword}/>
             </div>
             <div className="form-group">
               <label>Confirm the Password</label>
-              <input type="text" className="form-control" id="loginPassword" placeholder="Password" />
+              <input type="text" className="form-control" id="loginPassword" placeholder="Password" onChange={this.checkPassword}/>
             </div>
             <div className="form-group">
-              <Link to={process.env.PUBLIC_URL +'/'}><button type="button" class="btn btn-primary">Sign up</button></Link>
+              <button type="button" class="btn btn-primary" onClick={this.signUp}>Sign up</button>
             </div>
             <div className="form-group">
 
@@ -45,4 +58,5 @@ export class Login extends React.Component{
   }
 };
 
-export default Login;
+export default SignUp;
+<Link to={process.env.PUBLIC_URL +'/'}></Link>
