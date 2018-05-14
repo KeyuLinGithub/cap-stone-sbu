efd7e0ef44e8a121508190e5c9357a329414bcdf
Keyu Lin <mlchaellin222@gmail.com>
2018-05-13 23:03:39 -0400

    original/ current details

--- src/components/StateInfo.js
+++ src/components/StateInfo.js
@@ -32,6 +32,7 @@ class StateInfo extends React.Component {
    })
     .then(response => response.json())
     .then(data => {
+      console.log(data);
       this.setState({
         infoboxPopulation:data.population,
         infoboxAVGIncome:data.aveIncome,
